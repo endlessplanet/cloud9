498359378d4ce4480036bf610b5d3b0ee77d3bf0
Emory Merryman <emory.merryman@gmail.com>
2017-08-21 20:25:42 +0000

    no authentication

--- image/root/listen.sh
+++ image/root/listen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-forever /home/user/.c9/server.js -w /workspace/${PROJECT_NAME} --auth user:password --listen 0.0.0.0
+forever /home/user/.c9/server.js -w /workspace/${PROJECT_NAME} --listen 127.0.0.1
