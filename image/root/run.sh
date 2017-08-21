b9d3e11cb99a89862d9e458361c6a0e8f23f8059
Emory Merryman <emory.merryman@gmail.com>
2017-08-21 20:37:53 +0000

    do not use user user

--- image/root/run.sh
+++ image/root/run.sh
@@ -20,9 +20,5 @@ apk update &&
     mkdir /root/.ssh &&
     chmod 0700 /root/.ssh &&
     apk add --no-cache docker &&
-    adduser -D user &&
     mkdir /workspace &&
-    chown user:user /workspace &&
-    cp -r /root/.c9 /home/user/.c9 &&
-    chown user:user /home/user/.c9 &&
     rm -rf /var/cache/apk/*
