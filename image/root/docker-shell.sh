93faaf4b5115f446dea573bafa35c6b9a80492c4
Emory Merryman <emory.merryman@gmail.com>
2017-08-22 11:39:52 +0000

    some progress

--- image/root/docker-shell.sh
+++ image/root/docker-shell.sh
@@ -14,5 +14,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with cloud9 .  If not, see <http://www.gnu.org/licenses/>.
 
-echo docker exec --interactive --tty ${CONTAINER_ID} bash &&
-    bash
+sudo docker exec --interactive --tty ${CONTAINER_ID} bash
