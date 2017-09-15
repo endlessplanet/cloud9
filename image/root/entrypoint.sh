c61eb2f6be92259a437183350f92fa1b92e8ab7a
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 23:00:57 +0000

    reorg

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -16,21 +16,20 @@
 
 
 ssh-keygen -f /root/.ssh/id_rsa -P "" &&
-    SSHD_PORT=$(cat /root/.ssh/id_rsa.pub | docker \
-        container \
-        exec \
-        --interactive \
-        ${SSHD_CONTAINER} \
-        sh /opt/docker/reserve-ports.sh) &&
     docker \
         container \
         exec \
         --interactive \
         ${SSHD_CONTAINER} \
         ssh-keyscan sshd > /root/.ssh/known_hosts &&
-    sleep 3s &&
     chmod 0755 /root/.ssh/known_hosts &&
-    (nohup ssh -o StrictHostKeyChecking=no -fN -R 127.0.0.1:${SSHD_PORT}:127.0.0.1:8181 sshd </dev/null >/tmp/sshd1.log 2>&1 &) &&
-    (nohup ssh -o StrictHostKeyChecking=no -fN -L 0.0.0.0:80:0.0.0.0:${SSHD_PORT} sshd </dev/null >/tmp/sshd2.log 2>&1 &) &&
+    SSHD_PORT=$(cat /root/.ssh/id_rsa.pub | docker \
+        container \
+        exec \
+        --interactive \
+        ${SSHD_CONTAINER} \
+        sh /opt/docker/reserve-ports.sh) &&
+    (nohup ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fN -R 127.0.0.1:${SSHD_PORT}:127.0.0.1:8181 sshd </dev/null >/tmp/sshd1.log 2>&1 &) &&
+    (nohup ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fN -L 0.0.0.0:80:0.0.0.0:${SSHD_PORT} sshd </dev/null >/tmp/sshd2.log 2>&1 &) &&
     PROJECT_NAME=$(ls -1 /workspace) &&
     node /opt/docker/c9sdk/server.js -w /workspace/${PROJECT_NAME} ${@}
